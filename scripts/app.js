5fb0fb0e53ec321511f448e6a47d60f70af25e3e
Rohan Shrestha <shrestharohan039@gmail.com>
2022-07-10 00:31:42 +0545

    completed click navigation in main menu and optimized the code

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,17 @@
 const playerNameInputElement = document.getElementById('player-name');
 const menuOptionElements = document.querySelectorAll('.menu-option');
 const optionChoiceBtns = document.querySelectorAll('.option-choice-btn');
-const inputFieldSVG = document.getElementById('input-field-svg');
-const outlineSVGs = document.getElementsByClassName('h-6 w-6');
-const solidSVGs = document.getElementsByClassName('h-5 w-5');
+const outlineSVGs = document.getElementsByClassName('check-circle-outline');
+const solidSVGs = document.getElementsByClassName('check-circle-solid');
+const startGameBtn = document.getElementById('start-game-btn');
+const mainMenuElement = document.getElementById('main-menu');
 
 document.addEventListener('keydown', keyboardNavigationHandler);
 playerNameInputElement.addEventListener('input', inputFieldSVGToggler);
+
+for (const optionChoiceBtn of optionChoiceBtns) {
+  optionChoiceBtn.addEventListener('click', clickHandler);
+}
+
+menuOptionElements[0].addEventListener('click', clickHandler);
+startGameBtn.addEventListener('click', startGame);
--- scripts/main-menu.js
+++ scripts/main-menu.js
@@ -1,49 +1,54 @@
-// For Keyboard Navigation
 let selectedMenuOption = 0;
 let selectedOptionChoice = 0;
-let optionsSelected = 0;
+let optionsChecked = 0;
+
+function resetSVG(i) {
+  outlineSVGs[i].style.display = '';
+  solidSVGs[i].style.display = 'none';
+  if (i === 0) {
+    playerNameInputElement.value = '';
+    menuOptionElements[0].classList.remove('checked');
+  } else optionChoiceBtns[i - 1].classList.remove('checked');
+}
+
+function showSolidSVG(i) {
+  outlineSVGs[i].style.display = 'none';
+  solidSVGs[i].style.display = 'inline';
+  if (i === 0) menuOptionElements[0].classList.add('checked');
+  else optionChoiceBtns[i - 1].classList.add('checked');
+}
 
 function SVGChanger() {
-  if (selectedMenuOption === 1)
-    for (let i = 1; i < 4; i++) {
-      outlineSVGs[i].style.visibility = '';
-      solidSVGs[i].style.display = 'none';
-    }
-  else if (selectedMenuOption === 2)
-    for (let i = 4; i < 7; i++) {
-      outlineSVGs[i].style.visibility = '';
-      solidSVGs[i].style.display = 'none';
-    }
+  if (selectedMenuOption === 1) for (let i = 1; i < 4; i++) resetSVG(i);
+  else if (selectedMenuOption === 2) for (let i = 4; i < 7; i++) resetSVG(i);
 
-  if (selectedMenuOption === 0) {
-    if (playerNameInputElement.value !== '') {
-      outlineSVGs[0].style.visibility = 'hidden';
-      solidSVGs[0].style.display = 'inline';
-    }
-  } else
+  if (selectedMenuOption !== 0) {
+    if (playerNameInputElement.value !== '') showSolidSVG(0);
+
     for (let i = 1; i < outlineSVGs.length; i++)
       if (selectedOptionChoice + 1 === i) {
-        outlineSVGs[i].style.visibility = 'hidden';
-        solidSVGs[i].style.display = 'inline';
+        showSolidSVG(i);
         break;
       }
+  }
 }
 
 function inputFieldSVGToggler() {
-  if (playerNameInputElement.value.length > 0)
-    inputFieldSVG.style.visibility = 'visible';
-  else {
-    inputFieldSVG.style.visibility = 'hidden';
-    solidSVGs[0].style.display = 'none';
-  }
+  if (playerNameInputElement.value.length > 0) {
+    outlineSVGs[0].style.display = 'inline';
+    menuOptionElements[0].classList.add('checked');
+  } else resetSVG(0);
 }
 
 function toggleMenuSelection(eventKey) {
   menuOptionElements[selectedMenuOption].classList.remove('selected');
   if (eventKey === 'ArrowUp') {
     selectedMenuOption--;
-    if (optionsSelected > 0) optionsSelected--;
-  } else selectedMenuOption++;
+    if (selectedMenuOption === 0) resetSVG(0);
+  } else {
+    selectedMenuOption++;
+    if (playerNameInputElement.value !== '') showSolidSVG(0);
+  }
   menuOptionElements[selectedMenuOption].classList.add('selected');
 }
 
@@ -52,7 +57,6 @@ function toggleOptionChoiceSelection(eventKey) {
   if (eventKey === 'Enter') {
     if (selectedMenuOption === 1) selectedOptionChoice = 1;
     else if (selectedMenuOption === 2) selectedOptionChoice = 4;
-    if (selectedMenuOption !== 1) optionsSelected++;
   }
 
   if (selectedMenuOption > 0 && selectedMenuOption < 3) {
@@ -67,45 +71,72 @@ function toggleOptionChoiceSelection(eventKey) {
   }
 }
 
+function startGame() {
+  for (let i = 0; i < optionChoiceBtns.length; i++)
+    if (optionChoiceBtns[i].classList.contains('checked')) optionsChecked++;
+
+  if (playerNameInputElement.value !== '' && optionsChecked === 2) {
+    document.removeEventListener('keydown', keyboardNavigationHandler);
+    mainMenuElement.style.display = 'none';
+  } else {
+    optionsChecked = 0;
+    alert('Please fill and choose all the options.');
+  }
+}
+
 function keyboardNavigationHandler(event) {
   let optionSumIndex = selectedMenuOption + selectedOptionChoice;
 
-  if (event.key === 'ArrowDown') {
-    if (optionSumIndex === 0 || optionSumIndex === 3 || optionSumIndex === 7) {
-      toggleMenuSelection(event.key);
-      toggleOptionChoiceSelection(event.key);
-    } else {
-      toggleOptionChoiceSelection(event.key);
-    }
-  } else if (event.key === 'ArrowUp') {
+  // For Arrow Up and Arrow Down Keys
+  if (event.key === 'ArrowDown' || event.key === 'ArrowUp') {
+    const bool1 = [0, 3, 7].includes(optionSumIndex); // optionSumIndex === 0 || optionSumIndex === 3 || optionSumIndex === 7
+    const bool2 = [1, 5].includes(optionSumIndex) || selectedMenuOption === 3;
     if (
-      optionSumIndex === 1 ||
-      optionSumIndex === 5 ||
-      selectedMenuOption === 3
+      (event.key === 'ArrowDown' && bool1) ||
+      (event.key === 'ArrowUp' && bool2)
     ) {
       if (selectedMenuOption === 3) selectedOptionChoice = 5;
       toggleMenuSelection(event.key);
       toggleOptionChoiceSelection(event.key);
-    } else {
-      toggleOptionChoiceSelection(event.key);
-    }
-    if (selectedMenuOption === 0) {
-      playerNameInputElement.value = '';
-      inputFieldSVGToggler();
-    }
-  } else if (event.key === 'Enter') {
+    } else toggleOptionChoiceSelection(event.key);
+  }
+
+  // For Enter Key
+  else if (event.key === 'Enter') {
     if (selectedMenuOption < 3) {
       SVGChanger();
       toggleMenuSelection(event.key);
       toggleOptionChoiceSelection(event.key);
-    } else if (selectedMenuOption === 3) {
-      if (playerNameInputElement.value !== '' && optionsSelected === 2)
-        document.removeEventListener('keydown', keyboardNavigationHandler);
-    }
-  } else if (event.key === ' ') {
-    event.preventDefault();
+    } else if (selectedMenuOption === 3) startGame();
   }
+
+  // For Space Key (To not allow spaces in the player name)
+  else if (event.key === ' ') event.preventDefault();
 
   if (selectedMenuOption === 0) playerNameInputElement.focus();
   else playerNameInputElement.blur();
+}
+
+function clickHandler(event) {
+  const classList = event.target.classList;
+  menuOptionElements[selectedMenuOption].classList.remove('selected');
+  optionChoiceBtns[selectedOptionChoice].classList.remove('selected');
+  if (classList.contains('m-1')) selectedMenuOption = 1;
+  else if (classList.contains('m-2')) selectedMenuOption = 2;
+  else {
+    selectedMenuOption = 0;
+    selectedOptionChoice = 0;
+  }
+
+  for (let i = 0; i < optionChoiceBtns.length; i++)
+    if (event.target === optionChoiceBtns[i]) {
+      selectedOptionChoice = i;
+      optionChoiceBtns[selectedOptionChoice].classList.add('selected');
+      break;
+    }
+
+  menuOptionElements[selectedMenuOption].classList.add('selected');
+
+  if (selectedMenuOption === 0) resetSVG(0);
+  SVGChanger();
 }
