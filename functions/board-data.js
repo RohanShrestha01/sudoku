e712b1960ac4094efc152d3b8bf5a1ddc539329c
Rohan Shrestha <shrestharohan039@gmail.com>
2022-11-05 22:21:09 +0545

    replaced fetch api with axios

--- functions/board-data.js
+++ functions/board-data.js
@@ -1,8 +1,12 @@
+const axios = require('axios');
+
 exports.handler = async function (event) {
   const { difficulty } = event.queryStringParameters;
 
   const options = {
     method: 'GET',
+    url: `https://sudoku-board.p.rapidapi.com/new-board`,
+    params: { diff: difficulty, stype: 'string', solu: 'true' },
     headers: {
       'X-RapidAPI-Key': process.env.API_KEY,
       'X-RapidAPI-Host': 'sudoku-board.p.rapidapi.com',
@@ -10,15 +14,11 @@ exports.handler = async function (event) {
   };
 
   try {
-    const response = await fetch(
-      `https://sudoku-board.p.rapidapi.com/new-board?diff=${difficulty}&stype=string&solu=true`,
-      options
-    );
-    const data = await response.json();
+    const response = await axios(options);
 
     return {
       statusCode: 200,
-      body: JSON.stringify(data),
+      body: JSON.stringify(response.data),
     };
   } catch (error) {
     return {
